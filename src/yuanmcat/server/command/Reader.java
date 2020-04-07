32d6e80836a81d4c50e5eab07a255692c829e4cf
wuyuan1002 <2461190767@qq.com>
2020-04-07 14:51:09 +0800

    注释

--- src/yuanmcat/server/command/Reader.java
+++ src/yuanmcat/server/command/Reader.java
@@ -15,18 +15,18 @@ import java.nio.channels.SocketChannel;
  * @date 2019/12/28
  */
 public class Reader implements Handler {
-    //客户端监听事件的key
+    // 客户端监听事件的key
     private SelectionKey selectionKey;
     
-    //只有在有新的连接时才new一个Handler，执行该构造方法，并把handler绑定到key上。之后该连接有事件时会直接执行它绑定的handler的run方法
+    // 只有在有新的连接时才new一个Handler，执行该构造方法，并把handler绑定到key上。之后该连接有事件时会直接执行它绑定的handler的run方法
     public Reader(Selector selector, SocketChannel accept) {
         try {
-            //客户端连接通道
+            // 客户端连接通道
             accept.configureBlocking(false);
             selector.wakeup();
-            //把新的连接绑定到selector选择器上
+            // 把新的连接绑定到selector选择器上
             this.selectionKey = accept.register(selector, SelectionKey.OP_READ);
-            //给新的连接添加该Handler对象，用来处理该连接的事件
+            // 给新的连接添加该Handler对象，用来处理该连接的事件
             this.selectionKey.attach(this);
             selector.wakeup();
         } catch (IOException e) {
@@ -40,17 +40,17 @@ public class Reader implements Handler {
      */
     @Override
     public void execute() {
-        //获取连接通道
+        // 获取连接通道
         SocketChannel channel = (SocketChannel) this.selectionKey.channel();
         try {
             if (!(this.selectionKey.attachment() instanceof Sender) && this.selectionKey.isValid()) {
                 Request request = new Request(this.selectionKey);
                 if (request.getInvalidHeaderException() == null) {
-                    //是有效请求才注册写事件继续处理
+                    // 是有效请求才注册写事件继续处理
                     this.selectionKey.interestOps(SelectionKey.OP_WRITE);
                     this.selectionKey.attach(new Sender(this.selectionKey, request));
                 } else {
-                    //否则返回400,错误的请求
+                    // 否则返回400,错误的请求
                     HandleHttp.handleBadRequest(channel);
                     channel.close();
                 }
